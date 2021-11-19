3f864886f785789a1b9788000c242c04cce81e7f
MonadWizard <monad.wizard.r@gmail.com>
2021-11-19 19:29:05 +0600

    complete search price

--- product/views/product.py
+++ product/views/product.py
@@ -20,29 +20,24 @@ class CreateProductView(generic.TemplateView):
 def search(request):
     queryset_list = ProductVariantPrice.objects.order_by('-created_at')
 
-    # keywords
+    # title
     if 'title' in request.GET:
         title = request.GET['title']
         if title:
-            queryset_list = queryset_list.filter(product__title__contains=title)   # search enter description
-    # # citys
-    # if 'city' in request.GET:
-    #     city = request.GET['city']
-    #     if city:
-    #         queryset_list = queryset_list.filter(city__iexact=city)   # search enter description
-    #
-    # state
+            queryset_list = queryset_list.filter(product__title__contains=title)
+
+    # variant
     if 'variant' in request.GET:
         variant = request.GET['variant']
         if variant:
-            queryset_list = queryset_list.filter(product_variant_one__variant__title__contains=variant)   # search enter description
+            queryset_list = queryset_list.filter(product_variant_one__variant__title__contains=variant)
 
-    # # bedrooms
-    # if 'bedrooms' in request.GET:
-    #     bedrooms = request.GET['bedrooms']
-    #     if bedrooms:
-    #         queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)   # search enter description
-    #
+    # price
+
+    price_from = request.GET.get('price_from', 0)
+    price_to = request.GET.get('price_to', 1000000)
+    queryset_list = queryset_list.filter(price__gte=price_from).filter(price__lte=price_to)
+
     # # bedrooms
     # if 'price' in request.GET:
     #     price = request.GET['price']
@@ -54,6 +49,8 @@ def search(request):
     context = {
         # 'state_choices':state_choices,
         # 'bedroom_choices':bedroom_choices,
+        'price_from': price_from,
+        'price_to': price_to,
         'varient_choices':varient_choices,
         'listings':queryset_list,
         'values': request.GET,
